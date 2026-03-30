9cefe59c4184ea89b460537e3282393531426a24
Koray ÖZALP <korayozalp0660@gmail.com>
2026-03-30 23:40:16 +0200

    Feat: Add translation routes for API and web interface

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Controllers\TranslationController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
+Route::get('/translator', [TranslationController::class, 'index'])->name('translator.index');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
